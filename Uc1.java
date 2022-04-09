ed0b53b6c58d9f56931712d72ad13e9e69036206
SAROJ-2207 <sksaroj828@gmail.com>
2022-04-09 14:55:42 +0530

    Check Present Or Absent

---
+++ Uc1.java
@@ -0,0 +1,17 @@
+class Uc1
+{
+    public static void main(String arg[])
+    {
+        final int FULL_TIME = 1;
+        int empType = (int) (Math.random() * 100) % 2;
+        if (empType == FULL_TIME)
+        {
+            System.out.println("Employee is Present");
+
+        } else
+        {
+            System.out.println("Employee is Absent");
+
+        }
+    }
+}
